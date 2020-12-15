2dd6eb2680a629244e57a3ecdcb456c088a554ae
Lali-Pauline <76036640+Lali-Pauline@users.noreply.github.com>
2020-12-15 10:51:06 +0100

    Update binaryCounter.ino

--- binaryCounter.ino
+++ binaryCounter.ino
@@ -1,1 +1,19 @@
+//variables
+int pinSeven = 7;
+int light = 125;
 
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pinSeven, OUTPUT);
+ 
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  analogWrite(pinSeven, light);
+
+  
+ 
+}
